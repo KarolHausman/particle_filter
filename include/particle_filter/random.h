45946edeaf92c3030d179f52d2a6e98b7a77882c
Karol Hausman (CR/RTC1.1-NA) <hak2pal@shelob.(none)>
2014-06-30 20:31:44 -0700

    changing static class to namespaces

--- include/particle_filter/random.h
+++ include/particle_filter/random.h
@@ -4,25 +4,25 @@
 #include <Eigen/Core>
 
 
-class Random {
-public:
+namespace Random {
 
   //! initializes random seed
-  static void randomize();
+  void randomize();
 
   //! returns a sample of the uniform distribution in [0, 1]
-  static double uniform();
+  double uniform();
 
   //! returns a sample of the uniform distribution in [a, b]
-  static double uniform(double a, double b);
+  double uniform(double a, double b);
 
   //! returns a sample of the Gaussian distribution N(0, 1)
-  static double gaussian();
+  double gaussian();
 
-  static double gaussian(double mean, double stddev);
+  double gaussian(double mean, double stddev);
 
-  static Eigen::VectorXd multivariateGaussian(const Eigen::MatrixXd &covariance);
-};
+  Eigen::VectorXd multivariateGaussian(const Eigen::MatrixXd &covariance);
+
+}
 
 
 #endif /* RANDOM_H_ */
--- src/random.cpp
+++ src/random.cpp
@@ -3,41 +3,44 @@
 #include <unistd.h>
 #include <random>
 
-
-void Random::randomize() {
-  srand(time(NULL) + getpid());
-}
-
-double Random::uniform() {
-  return rand()/(double)RAND_MAX;
-}
-
-double Random::uniform(double a, double b) {
-  assert(b != a);
-  return a + (b-a)*uniform();
-}
-
-//! returns random sample of the gaussian N(0, 1) distribution
-double Random::gaussian()
+namespace Random
 {
-  std::default_random_engine generator;
-  std::normal_distribution <double> distribution(0.0,1.0);
 
-  return distribution(generator);
+  void randomize() {
+    srand(time(NULL) + getpid());
+  }
+
+  double uniform() {
+    return rand()/(double)RAND_MAX;
+  }
+
+  double uniform(double a, double b) {
+    assert(b != a);
+    return a + (b-a)*uniform();
+  }
+
+  //! returns random sample of the gaussian N(0, 1) distribution
+  double gaussian()
+  {
+    std::default_random_engine generator;
+    std::normal_distribution <double> distribution(0.0,1.0);
+
+    return distribution(generator);
+  }
+
+  double gaussian(double mean, double stddev) {
+    return gaussian()*stddev + mean;
+  }
+
+
+  Eigen::VectorXd multivariateGaussian(const Eigen::MatrixXd &covariance){
+    int size = covariance.rows();
+    Eigen::VectorXd sample(size);
+    for (int i=0; i<size; ++i)
+      sample(i) = gaussian();
+
+    Eigen::MatrixXd mL = covariance.llt().matrixL(); // Cholesky decomposition
+    return mL*sample;
+  }
+
 }
-
-double Random::gaussian(double mean, double stddev) {
-  return gaussian()*stddev + mean;
-}
-
-
-Eigen::VectorXd Random::multivariateGaussian(const Eigen::MatrixXd &covariance){
-  int size = covariance.rows();
-  Eigen::VectorXd sample(size);
-  for (int i=0; i<size; ++i)
-    sample(i) = gaussian();
-
-  Eigen::MatrixXd mL = covariance.llt().matrixL(); // Cholesky decomposition
-  return mL*sample;
-}
-
