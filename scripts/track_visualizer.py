099f189a689e2c649db92c4ca079de20c48ecabc
Karol Hausman (CR/RTC1.1-NA) <hak2pal@shelob.(none)>
2014-07-29 14:39:47 -0700

    prismatic joint visualization - orientation to be fixed

--- scripts/track_visualizer.py
+++ scripts/track_visualizer.py
@@ -10,7 +10,7 @@ import rospy
 import sys
 from articulation_model_msgs.msg import ModelMsg
 from visualization_msgs.msg import Marker,MarkerArray
-from geometry_msgs.msg import Vector3,Point
+from geometry_msgs.msg import Vector3, Point, Quaternion, Pose
 from std_msgs.msg import ColorRGBA
 from sensor_msgs.msg import ChannelFloat32
 import logging
@@ -52,6 +52,7 @@ class trackVisualizer:
     marker_array = MarkerArray()
     self.render_points(model.track,marker_array)
     
+    self.render_model(model, marker_array)
 
     #self.delete_old_markers(model.track,marker_array)	
 
@@ -59,7 +60,194 @@ class trackVisualizer:
     len(marker_array.markers) )
     self.pub_array.publish(marker_array)
 
-  def render_points(self,track,marker_array):
+  def render_model(self, model, marker_array):
+    
+    if model.name == "rotational":
+        self.render_rotational_model(model, marker_array)
+    if model.name == "prismatic":
+        self.render_prismatic_model(model, marker_array)
+
+
+  def render_prismatic_model(self, model, marker_array):
+    rigid_position_x = 0
+    rigid_position_y = 0
+    rigid_position_z = 0
+    rigid_orientation_x = 0
+    rigid_orientation_y = 0
+    rigid_orientation_z = 0
+    rigid_orientation_w = 0
+    prismatic_dir_x = 0
+    prismatic_dir_y = 0
+    prismatic_dir_z = 0
+    for param in model.params:
+      if param.name == "rigid_position.x":
+        rigid_position_x = param.value
+      if param.name == "rigid_position.y":
+        rigid_position_y = param.value
+      if param.name == "rigid_position.z":
+        rigid_position_z = param.value
+
+      if param.name == "rigid_orientation.x":
+        rigid_orientation_x = param.value
+      if param.name == "rigid_orientation.y":
+        rigid_orientation_y = param.value
+      if param.name == "rigid_orientation.z":
+        rigid_orientation_z = param.value
+      if param.name == "rigid_orientation.w":
+        rigid_orientation_w = param.value
+
+      if param.name == "prismatic_dir.x":
+        prismatic_dir_x = param.value
+      if param.name == "prismatic_dir.y":
+        prismatic_dir_y = param.value
+      if param.name == "prismatic_dir.z":
+        prismatic_dir_z = param.value
+
+      rigid_pose_orientation = Quaternion(rigid_orientation_x, rigid_orientation_y, rigid_orientation_z, rigid_orientation_w)          
+      rigid_pose_position = Point(rigid_position_x, rigid_position_y, rigid_position_z)
+      rigid_pose = Pose(rigid_pose_position, rigid_pose_orientation)
+      prismatic_dir = Point(prismatic_dir_x, prismatic_dir_y, prismatic_dir_z)
+
+    marker = Marker()
+    marker.header.stamp = model.track.header.stamp
+    marker.header.frame_id = model.track.header.frame_id
+    marker.ns = "model_visualizer"
+    marker.id = 0#self.num_markers[model.track.id]
+    marker.action = Marker.ADD
+
+    marker.scale = Vector3(0.01,0.01,0.01)
+    #marker.color = self.generate_color_axis(track.id, i,axis)
+    marker.color.a = 1
+    marker.color.b = 1
+
+    marker.type = Marker.LINE_STRIP
+    marker.pose = rigid_pose
+    for axis in range(3):
+      marker.points.append( Point(0,0,0) )
+      if axis==0:
+        marker.points.append( Point(0.3,0,0) )
+      elif axis==1:
+        marker.points.append( Point(0,0.3,0) )
+      elif axis==2:
+        marker.points.append( Point(0,0,0.3) )
+
+    marker_array.markers.append(marker)
+
+    #direction
+    marker_dir = Marker()
+    marker_dir.header.stamp = model.track.header.stamp
+    marker_dir.header.frame_id = model.track.header.frame_id
+    marker_dir.ns = "model_visualizer_dir"
+    marker_dir.id = 0#self.num_markers[model.track.id]
+    marker_dir.action = Marker.ADD
+
+    marker_dir.scale = Vector3(0.01,0.01,0.01)
+    marker_dir.color.a = 1
+    marker_dir.color.r = 1
+
+    marker_dir.type = Marker.LINE_STRIP
+    marker_dir.pose = rigid_pose
+    marker_dir.points.append( Point(0,0,0) )
+    marker_dir.points.append( prismatic_dir )
+    
+    marker_array.markers.append(marker_dir)
+
+
+  def render_rotational_model(self, model, marker_array):
+    rot_radius = 0
+    rot_center_x = 0
+    rot_center_y = 0
+    rot_center_z = 0
+    rot_orientation_x = 0
+    rot_orientation_y = 0
+    rot_orientation_z = 0
+    rot_orientation_w = 0
+    rot_axis_x = 0
+    rot_axis_y = 0
+    rot_axis_z = 0
+    rot_axis_w = 0
+
+    for param in model.params:
+      if param.name == "rot_radius":
+        rot_radius = param.value
+      if param.name == "rot_center.x":
+        rot_center_x = param.value
+      if param.name == "rot_center.y":
+        rot_center_y = param.value
+      if param.name == "rot_center.z":
+        rot_center_z = param.value
+
+      if param.name == "rot_orientation.x":
+        rot_orientation_x = param.value
+      if param.name == "rot_orientation.y":
+        rot_orientation_y = param.value
+      if param.name == "rot_orientation.z":
+        rot_orientation_z = param.value
+      if param.name == "rot_orientation.w":
+        rot_orientation_w = param.value
+
+      if param.name == "rot_axis.x":
+        rot_axis_x = param.value
+      if param.name == "rot_axis.y":
+        rot_axis_y = param.value
+      if param.name == "rot_axis.z":
+        rot_axis_z = param.value
+      if param.name == "rot_axis.w":
+        rot_axis_w = param.value
+
+      rot_axis = Quaternion(rot_axis_x, rot_axis_y, rot_axis_z, rot_axis_w) 
+      rot_center_position = Point(rot_center_x, rot_center_y, rot_center_z)
+      rot_center_orientation = Quaternion(rot_orientation_x, rot_orientation_y, rot_orientation_z, rot_orientation_w)          
+      rot_center = Pose(rot_center_position, rot_axis)#rot_center_orientation)
+        
+    marker = Marker()
+    marker.header.stamp = model.track.header.stamp
+    marker.header.frame_id = model.track.header.frame_id
+    marker.ns = "model_visualizer"
+    marker.id = 0#self.num_markers[model.track.id]
+    marker.action = Marker.ADD
+
+    marker.scale = Vector3(0.01,0.01,0.01)
+    #marker.color = self.generate_color_axis(track.id, i,axis)
+    marker.color.a = 1
+    marker.color.b = 1
+
+    marker.type = Marker.LINE_STRIP
+    marker.pose = rot_center
+
+        
+    for axis in range(3):
+      marker.points.append( Point(0,0,0) )
+      if axis==0:
+        marker.points.append( Point(0.3,0,0) )
+      elif axis==1:
+        marker.points.append( Point(0,0.3,0) )
+      elif axis==2:
+        marker.points.append( Point(0,0,0.3) )
+
+    marker_array.markers.append(marker)
+
+    #adding radius
+    marker_radius = Marker()
+    marker_radius.header.stamp = model.track.header.stamp
+    marker_radius.header.frame_id = model.track.header.frame_id
+    marker_radius.ns = "model_visualizer_radius"
+    marker_radius.id = 0#self.num_markers[model.track.id]
+    marker_radius.action = Marker.ADD
+
+    marker_radius.scale = Vector3(0.01,0.01,0.01)
+    marker_radius.color.a = 1
+    marker_radius.color.r = 1
+
+    marker_radius.type = Marker.LINE_STRIP
+    marker_radius.pose = rot_center
+    marker_radius.points.append( Point(0,0,0) )
+    marker_radius.points.append( Point(rot_radius,0,0) )
+    
+    marker_array.markers.append(marker_radius)
+      
+
+  def render_points(self, track, marker_array):
     for i in range( len( track.pose ) ):
       marker = Marker()
       marker.header.stamp = track.header.stamp
--- src/model_test.cpp
+++ src/model_test.cpp
@@ -14,6 +14,7 @@
 #include "articulation_model_msgs/ParamMsg.h"
 #include "particle_filter/articulation_model.h"
 #include "particle_filter/rotational_model.h"
+#include "particle_filter/prismatic_model.h"
 #include "particle_filter/rigid_model.h"
 
 
@@ -27,7 +28,7 @@ int main(int argc, char** argv)
   ros::Rate loop_rate(5);
   int count = 0;
 
-  boost::normal_distribution<> nd(0.0, 0.02);
+  boost::normal_distribution<> nd(0.0, 0.01);
   boost::mt19937 rng;
   boost::variate_generator<boost::mt19937&, boost::normal_distribution<> >
                   var_nor(rng, nd);
@@ -40,6 +41,7 @@ int main(int argc, char** argv)
   {
     articulation_model_msgs::ModelMsg model_msg;
 //    model_msg.name = "rotational";
+    model_msg.name = "prismatic";
     articulation_model_msgs::ParamMsg sigma_param;
     sigma_param.name = "sigma_position";
     sigma_param.value = 0.02;
@@ -50,12 +52,12 @@ int main(int argc, char** argv)
     model_msg.track.header.frame_id = "/world";
 //    model_msg.track.track_type = articulation_model_msgs::TrackMsg::TRACK_POSITION_ONLY;
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 300; i++)
     {
       geometry_msgs::Pose pose;
-      pose.position.x = cos(i / 100.0 + count / 10.0) + var_nor();
-      pose.position.y = sin(i / 100.0 + count / 10.0)+ var_nor();
-      pose.position.z = var_nor();
+      pose.position.x = (static_cast<float> (i)/100.0) + var_nor()/*cos(i / 100.0 + count / 10.0) + var_nor()*/;
+      pose.position.y = /*sin(i / 100.0 + count / 10.0)+*/(static_cast<float> (i)/100.0) + var_nor();
+      pose.position.z = (static_cast<float> (i)/100.0) + var_nor();
       pose.orientation.x = 0;
       pose.orientation.y = 0;
       pose.orientation.z = 0;
@@ -66,7 +68,8 @@ int main(int argc, char** argv)
     model_msg.track.header.seq = 0;
 
     std::cout << "creating object" << std::endl;
-    ArticulationModelPtr model_instance(new RotationalModel);//factory.restoreModel(model_msg);
+//    ArticulationModelPtr model_instance(new RotationalModel);//factory.restoreModel(model_msg);
+    ArticulationModelPtr model_instance(new PrismaticModel);
     model_instance->setModel(model_msg);
 
     std::cout << "fitting" << std::endl;
@@ -78,28 +81,29 @@ int main(int argc, char** argv)
 
 
 //    std::cout << "model class = "<< model_instance->getModelName() << std::endl;
-    std::cout << "       radius = "<<model_instance->getParam("rot_radius")<< std::endl;
 
-    boost::shared_ptr<RotationalModel> rotational = boost::dynamic_pointer_cast< RotationalModel > (model_instance);
-    std::cout << "    rotational   radius = "<<rotational->rot_radius<< std::endl;
+//    std::cout << "       radius = "<<model_instance->getParam("rot_radius")<< std::endl;
 
-    tf::Matrix3x3 m(rotational->rot_axis);
-    double yaw, pitch, roll;
-    m.getEulerYPR(yaw, pitch, roll);
-    std::cout << "    rotational   yaw = "<<yaw<< std::endl;
-    std::cout << "    rotational   pitch = "<<pitch<< std::endl;
-    std::cout << "    rotational   roll = "<<roll<< std::endl;
+//    boost::shared_ptr<RotationalModel> rotational = boost::dynamic_pointer_cast< RotationalModel > (model_instance);
+//    std::cout << "    rotational   radius = "<<rotational->rot_radius<< std::endl;
 
-
-//    std::cout << "       rigid_position.x = "<<model_instance->getParam("rigid_position.x")<< std::endl;
-//    boost::shared_ptr<RigidModel> rigid = boost::dynamic_pointer_cast< RigidModel > (model_instance);
-//    std::cout << "     pos.x = " << rigid->pos_x << std::endl;
+//    tf::Matrix3x3 m(rotational->rot_axis);
+//    double yaw, pitch, roll;
+//    m.getEulerYPR(yaw, pitch, roll);
+//    std::cout << "    rotational   yaw = "<<yaw<< std::endl;
+//    std::cout << "    rotational   pitch = "<<pitch<< std::endl;
+//    std::cout << "    rotational   roll = "<<roll<< std::endl;
 
 
+////    std::cout << "       rigid_position.x = "<<model_instance->getParam("rigid_position.x")<< std::endl;
+////    boost::shared_ptr<RigidModel> rigid = boost::dynamic_pointer_cast< RigidModel > (model_instance);
+////    std::cout << "     pos.x = " << rigid->pos_x << std::endl;
 
-    std::cout << "       center.x = "<<model_instance->getParam("rot_center.x")<< std::endl;
-    std::cout << "       center.y = "<<model_instance->getParam("rot_center.y")<< std::endl;
-    std::cout << "       center.z = "<<model_instance->getParam("rot_center.z")<< std::endl;
+
+
+//    std::cout << "       center.x = "<<model_instance->getParam("rot_center.x")<< std::endl;
+//    std::cout << "       center.y = "<<model_instance->getParam("rot_center.y")<< std::endl;
+//    std::cout << "       center.z = "<<model_instance->getParam("rot_center.z")<< std::endl;
     std::cout << "       log LH = " << model_instance->getParam("loglikelihood") << std::endl; //TODO: change back to getLikelihood
     std::cout << " points= "<<model_msg.track.pose.size()<< std::endl;
 
@@ -108,6 +112,6 @@ int main(int argc, char** argv)
 
     ros::spinOnce();
     loop_rate.sleep();
-    ++count;
+//    ++count;
   }
 }
